69fc5e82182dadd159a185625af2cd0bdda7de69
Dara Heaphy <23369914@studentmail.ul.ie>
2023-12-20 09:36:05 +0000

    final commit

--- demoTaxiApp/src/main/java/VehicleHiringTest.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-public interface VehicleHiringTest {
-    public abstract boolean testAddToMap(String reg, Location loc);
-    public abstract boolean testMoveVehicle(String reg, Location loc);
-    public abstract boolean testRemoveVehicle(String reg);
-    public abstract Location testGetVehicleLoc(String reg);
-    public abstract List<String> testGetVehiclesInRange(Location loc, int r);
-}
